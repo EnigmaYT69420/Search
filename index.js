e6b437a5ef04ab798cbb945cb2f04f5353bff213
EnigmaYT69420 <105698231+EnigmaYT69420@users.noreply.github.com>
2022-06-03 00:01:17 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,143 @@
+$(document).ready(function(){
+  $(".ab").click(function(){
+    $(".a").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".bb").click(function(){
+    $(".b").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".cb").click(function(){
+    $(".c").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".db").click(function(){
+    $(".d").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".eb").click(function(){
+    $(".e").fadeIn();
+  });
+});$(document).ready(function(){
+  $(".fb").click(function(){
+    $(".f").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".gb").click(function(){
+    $(".g").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".hb").click(function(){
+    $(".h").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".ib").click(function(){
+    $(".i").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".jb").click(function(){
+    $(".j").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".kb").click(function(){
+    $(".k").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".lb").click(function(){
+    $(".l").fadeIn();
+  });
+});$(document).ready(function(){
+  $(".mb").click(function(){
+    $(".m").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".nb").click(function(){
+    $(".n").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".ob").click(function(){
+    $(".o").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".pb").click(function(){
+    $(".p").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".qb").click(function(){
+    $(".q").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".rb").click(function(){
+    $(".r").fadeIn();
+  });
+});
+
+
+$(document).ready(function(){
+  $(".sb").click(function(){
+    $(".s").fadeIn();
+  });
+});$(document).ready(function(){
+  $(".tb").click(function(){
+    $(".t").fadeIn();
+  });
+});$(document).ready(function(){
+  $(".ub").click(function(){
+    $(".u").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".vb").click(function(){
+    $(".v").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".wb").click(function(){
+    $(".w").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".xb").click(function(){
+    $(".x").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".yb").click(function(){
+    $(".y").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $(".zb").click(function(){
+    $(".z").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $("#show").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $("").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
+  });
+});
+
+
+
