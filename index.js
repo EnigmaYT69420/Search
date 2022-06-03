a67a586abf17636c9d24b79edbdbfcbe96813e16
EnigmaYT69420 <105698231+EnigmaYT69420@users.noreply.github.com>
2022-06-03 14:47:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,88 +1,106 @@
 $(document).ready(function(){
   $(".ab").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".a").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".bb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".b").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".cb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".c").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".db").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".d").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".eb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".e").fadeIn();
   });
 });$(document).ready(function(){
   $(".fb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".f").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".gb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".g").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".hb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".h").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".ib").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".i").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".jb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".j").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".kb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".k").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".lb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".l").fadeIn();
   });
 });$(document).ready(function(){
   $(".mb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".m").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".nb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".n").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".ob").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".o").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".pb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".p").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".qb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".q").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".rb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".r").fadeIn();
   });
 });
@@ -90,54 +108,62 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   $(".sb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".s").fadeIn();
   });
 });$(document).ready(function(){
   $(".tb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".t").fadeIn();
   });
 });$(document).ready(function(){
   $(".ub").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".u").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".vb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".v").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".wb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".w").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".xb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".x").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".yb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".y").fadeIn();
   });
 });
 $(document).ready(function(){
   $(".zb").click(function(){
+    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
     $(".z").fadeIn();
   });
 });
 
 $(document).ready(function(){
   $("#show").click(function(){
-    $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeIn();
+    $(".b, .g, .h, .m, .r, .s, .t, .u, .z").fadeIn();
   });
 });
 
+
 $(document).ready(function(){
-  $("").click(function(){
+  $("#clear").click(function(){
     $(".a, .b, .c, .d, .e, .f, .g, .h, .i, .j, .k, .l, .m, .n, .o, .p, .q, .r, .s, .t, .u, .v, .w, .x, .y, .z").fadeOut();
   });
 });
-
 
 
